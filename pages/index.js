48ce9eeb09c690e9ff176b374d7725617d45e930
sakil-ahmed <sakilahmed1495@gmail.com>
2023-01-17 12:28:33 +0600

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Header from "@/Components/Header/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home</title>
+      </Head>
       <Header />
     </>
   );
